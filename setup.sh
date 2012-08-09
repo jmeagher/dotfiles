a74e9f588e97202ab85e89a4930649b357ee4ee8
jmeagher <jmeagher@lotame.com>
2012-08-09 11:08:56 -0400

    Get bin linking working in the setup script

--- setup.sh
+++ setup.sh
@@ -1,26 +1,34 @@
 
 function linkit() {
-    tgt=~/.$1
+    tgt=~/$2
+    src=$1
 
-    if [ ! -e $1 ] ; then
-        echo "I can't find $1, check your code"
+
+    if [ ! -e $src ] ; then
+        echo "I can't find $src, check your code"
         exit 1
     fi
 
     if [ -e $tgt -a ! -h $tgt ] ; then
-        echo "$1 looks like it is already setup... you should delete it first or move it so I don't break things"
+        echo "$src looks like it is already setup... you should delete it first or move it so I don't break things"
         exit 1
     fi
     if [ -e $tgt -a -h $tgt ] ; then
-        echo "Removing old symbolic link for $1"
+        echo "Removing old symbolic link for $src"
         rm $tgt
     fi
 
-    echo "Adding link for $1"
-    ln -s `pwd`/$1 $tgt
+    echo "Adding link for $src"
+    ln -s `pwd`/$src $tgt
 }
 
-linkit bashrc
-linkit bash_profile
-linkit vimrc
+linkit bashrc .bashrc
+linkit bash_profile .bash_profile
+linkit vimrc .vimrc
+
+# Link everything from bin to ~/bin
+for f in `ls bin/*` ; do
+    linkit $f $f
+done
+
 
