b06bfc98b65c1907b9f77f016e095e30a2c604f3
jmeagher <jmeagher@lotame.com>
2012-10-22 13:05:28 -0400

    Fix the bootstrap template to actually work

--- ec2/bootstrap.template.sh
+++ ec2/bootstrap.template.sh
@@ -6,11 +6,15 @@ export EC2_INSTANCE_ID="`wget -q -O - http://169.254.169.254/latest/meta-data/in
 export AWS_ACCESS_KEY=__AWS_ACCESS_KEY__
 export AWS_SECRET_KEY=__AWS_SECRET_KEY__
 
+export EC2_HOME=/opt/aws/apitools/ec2
+export JAVA_HOME=/usr/lib/jvm/jre
+export PATH=$PATH:$EC2_HOME/bin:$JAVA_HOME/bin
+
 for TAG in __TAG_LIST__ ; do
     ec2addtag $EC2_INSTANCE_ID -tag $TAG
 done
 
-if [ "" !- "__YUM_LIST__" ] ; then
+if [ "" != "__YUM_LIST__" ] ; then
     yum -y install __YUM_LIST__
 fi
 
