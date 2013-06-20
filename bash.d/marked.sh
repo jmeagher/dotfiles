fb861d197ef6942eba376c080268dc0839f6b154
John Meagher <john.meagher@livingsocial.com>
2013-06-20 17:44:13 -0400

    Add Marked CLI utility support

---
+++ bash.d/marked.sh
@@ -0,0 +1,7 @@
+
+# Helper command line utility for http://markedapp.com/
+if [ -e /Applications/Marked.app/Contents/Resources/mark ] ; then
+    export PATH=$PATH:/Applications/Marked.app/Contents/Resources
+fi
+
+
