56fa700ec564791c9323b700f776e20085de86d2
John Meagher <john.meagher@livingsocial.com>
2013-06-27 10:58:01 -0400

    Add a little ruby setup and better bash completion

--- bash.d/bash_completion.sh
+++ bash.d/bash_completion.sh
@@ -1,6 +1,12 @@
 
+# Run the system wide completion setup
 for file in /usr/local/etc/bash_completion /opt/local/etc/bash_completion /etc/bash_completion ; do
 	[ -r "$file" ] && source "$file"
+done
+
+# run any user completion setup
+for file in ~/.bash_completion.d/* ; do
+    source $file
 done
 unset file
 
