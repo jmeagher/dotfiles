0d4c025719b9b71c85559b07c8d2c5544b4948a0
John Meagher <john.meagher@livingsocial.com>
2013-07-19 14:43:01 -0400

    rbenv helper

--- bash.d/ruby.sh
+++ bash.d/ruby.sh
@@ -1,4 +1,30 @@
 if [ "" != "`which rbenv 2> /dev/null `" ] ; then
     eval " $(rbenv init - )"
+
+    function rbenvsetup() {
+      if [ ! -e .ruby-version ] ; then
+          rbenv global > .ruby-version 
+      fi
+
+
+      rbenv gemset version 2>&1 > /dev/null
+      r=$?
+      if [ $r -eq 0 ] ; then
+          if [ ! -e .ruby-gemset ] ; then
+              echo "Enter gemset name"
+              read gs
+              echo $gs > .ruby-gemset
+              unset gs
+          fi
+      fi
+
+
+      echo "Install bundler? ctrl-c to quit, enter to continue"
+      read a
+      echo "Ok, installing..."
+      gem install bundler --no-rdoc --no-ri
+      unset a
+
+    }
 fi
 
