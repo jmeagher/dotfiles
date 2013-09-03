218e9612db078b010f4b74f5a91699dd72b8cc83
John Meagher <john.meagher@livingsocial.com>
2013-09-03 13:56:21 -0400

    Nifty python based json formatting

--- bash.d/python.sh
+++ bash.d/python.sh
@@ -3,4 +3,8 @@ if [ "" != "`which python 2> /dev/null `" ] ; then
     function pyserver() {
         python -m SimpleHTTPServer
     }
+
+    # Pretty print json: echo '{"json":{"foo":"bar", "baz":"blah"}}'  | jsonpp
+    alias jsonpp="python -m json.tool"
 fi
+
