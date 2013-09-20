b8f3822ea82bf7dda078bcc30092c2d0c6a239a6
John Meagher <john.meagher@livingsocial.com>
2013-09-20 14:23:55 -0400

    Add some go setup and utilities

---
+++ bash.d/go.sh
@@ -0,0 +1,18 @@
+
+# Various setup things related to golang
+
+if [ "" != "`which go 2> /dev/null `" ] ; then
+
+    if [ "" = "$GOPATH" ] ; then
+        GOPATH=~/.gocode
+        if [ ! -e $GOPATH ] ; then
+            mkdir -p $GOPATH
+        fi
+
+        PATH=$PATH:$GOPATH/bin
+
+        export GOPATH
+        export PATH
+    fi
+fi
+
