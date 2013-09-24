eee0648ea52c5a474adb8716d678852aed7fc9ff
John Meagher <john.meagher@livingsocial.com>
2013-09-24 18:07:14 -0400

    Bash help helper

--- bash.d/bash_extras.sh
+++ bash.d/bash_extras.sh
@@ -1,3 +1,6 @@
 alias sumit="awk '{s+=\$1} END {print s}'"
 alias fgr="grep -riHn"
 
+# Nifty helper from @onelinetips and @jakehofman
+alias gtip="curl -s jakehofman.com/lists/tips.txt | grep -i"
+
