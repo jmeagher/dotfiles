ea91ce6050b9e609c388e0b60c62edfacdeb2601
John Meagher <john.meagher@livingsocial.com>
2014-01-29 10:56:04 -0500

    Minor slate updates

--- osx/slate/base-slate.js
+++ osx/slate/base-slate.js
@@ -13,51 +13,59 @@ S.cfga({
 
 
 var wrap = function(op) {
-    return {
+  return {
     "operations" : [ op ],
     "ignore-fail" : true,
     "repeat" : true
-};
+  };
 }
 
 var w = wrap;
 
 var moveOnly = function(theScreen, xpos, ypos) {
-    return  S.op("move", {
-            "screen" : theScreen,
-            "x" : xpos + "*screenSizeX + screenOriginX",
-            "y" : ypos + "*screenSizeY + screenOriginY",
-            "width"  : "windowSizeX",
-            "height" : "windowSizeY"
-        });
+  return  S.op("move", {
+      "screen" : theScreen,
+      "x" : xpos + "*screenSizeX + screenOriginX",
+      "y" : ypos + "*screenSizeY + screenOriginY",
+      "width"  : "windowSizeX",
+      "height" : "windowSizeY"
+    });
 }
 
 var moveSize = function(theScreen, xpos, ypos, width, height) {
-    return S.op("move", {
-            "screen" : theScreen,
-            "x" : xpos + "*screenSizeX + screenOriginX",
-            "y" : ypos + "*screenSizeY + screenOriginY",
-            "width"  : width + "*screenSizeX",
-            "height" : height + "*screenSizeY",
-        });
+  return S.op("move", {
+      "screen" : theScreen,
+      "x" : xpos + "*screenSizeX + screenOriginX",
+      "y" : ypos + "*screenSizeY + screenOriginY",
+      "width"  : width + "*screenSizeX",
+      "height" : height + "*screenSizeY",
+    });
 }
 
 var cornerOnly = function(theScreen, corner) {
-    return S.op("corner", {
-            "screen" : theScreen,
-            "direction" : corner,
-            "width"  : "windowSizeX",
-            "height" : "windowSizeY"
-        });
+  return S.op("corner", {
+      "screen" : theScreen,
+      "direction" : corner,
+      "width"  : "windowSizeX",
+      "height" : "windowSizeY"
+    });
 }
 
 var cornerSize = function(theScreen, corner, width, height) {
-    return S.op("corner", {
-            "screen" : theScreen,
-            "direction" : corner,
-            "width"  : width + "*screenSizeX",
-            "height" : height + "*screenSizeY",
-        });
+  return S.op("corner", {
+      "screen" : theScreen,
+      "direction" : corner,
+      "width"  : width + "*screenSizeX",
+      "height" : height + "*screenSizeY",
+    });
+}
+
+var sideOnly = function(theScreen, side) {
+  return S.op("push", {
+      "screen" : theScreen,
+      "direction" : side,
+      "style" : "center"
+    });
 }
 
 /*
@@ -72,10 +80,10 @@ var byTitleRegex = function(actionPairs, defaultOperation) {
 
       if (title !== undefined)
       {
-          var matched = _.find(actionPairs, function(it) { return title.match(it["r"]); });
-          if (matched) {
-             operation = matched["op"];
-          }
+        var matched = _.find(actionPairs, function(it) { return title.match(it["r"]); });
+        if (matched) {
+           operation = matched["op"];
+        }
       }
       
       windowObject.doOperation(operation);
