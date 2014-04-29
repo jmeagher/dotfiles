7598de7b726fbcbc780baf9ba87887c97beb65a5
John Meagher <john.meagher@livingsocial.com>
2014-04-29 14:46:10 -0400

    Prompt update to include ruby info

--- bash.d/bash_prompt.sh
+++ bash.d/bash_prompt.sh
@@ -3,68 +3,79 @@
 
 
 parse_git_branch() {
-   GIT_BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/git:\1/'`
-   # Can't figure out how to get this to work with the built in OSX sed
-   GIT_EXTRA=`git status --porcelain 2> /dev/null | cut -c 1-2 | tr "?" "N" | extsed 's/(.)/-\1/g' | tr "-" "\n" | sort | uniq | tr "\n" " " | sed "s/ //g"`
-   if [ "$GIT_EXTRA" = "" ] ; then
-       echo "$GIT_BRANCH"
-   else
-       echo "$GIT_BRANCH (${GIT_EXTRA})"
-   fi
+  GIT_BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/git:\1/'`
+  # Can't figure out how to get this to work with the built in OSX sed
+  GIT_EXTRA=`git status --porcelain 2> /dev/null | cut -c 1-2 | tr "?" "N" | extsed 's/(.)/-\1/g' | tr "-" "\n" | sort | uniq | tr "\n" " " | sed "s/ //g"`
+  if [ "$GIT_EXTRA" = "" ] ; then
+    echo "$GIT_BRANCH"
+  else
+    echo "$GIT_BRANCH (${GIT_EXTRA})"
+  fi
 }
 
 parse_aws_info() {
-    # To enable this the _AWS_CHECK variable will need to be defined somewhere else
-    if [ "$_AWS_CHECK" = "" ] ; then
+  # To enable this the _AWS_CHECK variable will need to be defined somewhere else
+  if [ "$_AWS_CHECK" = "" ] ; then
 
-        TRY_EC2=false
-        # Note, this is not the best check, but it's quick and should work for me
-        # Need to get this to run once, but haven't figured it out yet
-        uname -a | grep amzn >& /dev/null && TRY_EC2=true
-        if [ "$TRY_EC2" = "true" ] ; then
-            AWS_PROMPT=AWS
-            # The instance printing proved too slow, just print AWS instead
-        #    EC2_INSTANCE_ID="`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`"
-        #    INSTANCE_NAME="`ec2-describe-instances $EC2_INSTANCE_ID | grep TAG | grep Name | awk '{print $5}'`"
-        #    if [ "$INSTANCE_NAME" != "" ] ; then
-        #        AWS_PROMPT="AWS $INSTANCE_NAME $EC2_INSTANCE_ID"
-        #    fi
-        fi
-
-        _AWS_CHECK=true
+    TRY_EC2=false
+    # Note, this is not the best check, but it's quick and should work for me
+    # Need to get this to run once, but haven't figured it out yet
+    uname -a | grep amzn >& /dev/null && TRY_EC2=true
+    if [ "$TRY_EC2" = "true" ] ; then
+      AWS_PROMPT=AWS
+      # The instance printing proved too slow, just print AWS instead
+    #    EC2_INSTANCE_ID="`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`"
+    #    INSTANCE_NAME="`ec2-describe-instances $EC2_INSTANCE_ID | grep TAG | grep Name | awk '{print $5}'`"
+    #    if [ "$INSTANCE_NAME" != "" ] ; then
+    #        AWS_PROMPT="AWS $INSTANCE_NAME $EC2_INSTANCE_ID"
+    #    fi
     fi
-    echo -n $AWS_PROMPT
+
+    _AWS_CHECK=true
+  fi
+  echo -n $AWS_PROMPT
+}
+
+parse_ruby_info() {
+  _RV=$(rbenv version | cut -f1 -d ' ')
+  if [ "" != "$_RV" ] ; then
+    _GS=$(rbenv gemset active 2>&1 | grep -v "no active gemse" | sed "s/\bglobal\b//" | sed "s/ $//")
+    if [ "$_GS" != "" ] ; then
+      _RV="$_RV($_GS)"
+    fi
+    echo "rbenv:$_RV"
+  fi
 }
 
 parse_last_command_error() {
-    code=$?
-    if [ "$code" != "0" ] ; then
-        echo "Err ◕︵◕ $code"
-    fi
+  code=$?
+  if [ "$code" != "0" ] ; then
+    echo "Err ◕︵◕ $code"
+  fi
 }
 
 __DO_UNICODE=false
 [[ "$(locale charmap)" = "UTF-8" ]] && __DO_UNICODE=true
 export __DO_UNICODE
 prompt_time() {
-    p_icon=
-    if [ "$__DO_UNICODE" = "true" ] ; then
-        declare -i t_hour=$(date +%H%M | sed 's/^0*//')
-        if [[ $t_hour -lt 0600 ]] ; then 
-            p_icon="☣  "
-        elif [[ $t_hour -lt 1100 ]] ; then 
-            p_icon="☕  "
-        elif [[ $t_hour -lt 1330 ]] ; then
-            p_icon="🍔  "
-        elif [[ $t_hour -lt 1600 ]] ; then
-            p_icon="☀  "
-        elif [[ $t_hour -lt 2200 ]] ; then 
-            p_icon="🍺  "
-        else
-            p_icon="☣  "
-        fi
+  p_icon=
+  if [ "$__DO_UNICODE" = "true" ] ; then
+    declare -i t_hour=$(date +%H%M | sed 's/^0*//')
+    if [[ $t_hour -lt 0600 ]] ; then 
+      p_icon="☣  "
+    elif [[ $t_hour -lt 1100 ]] ; then 
+      p_icon="☕  "
+    elif [[ $t_hour -lt 1330 ]] ; then
+      p_icon="🍔  "
+    elif [[ $t_hour -lt 1600 ]] ; then
+      p_icon="☀  "
+    elif [[ $t_hour -lt 2200 ]] ; then 
+      p_icon="🍺  "
+    else
+      p_icon="☣  "
     fi
-    echo -n "${p_icon}$(date +%Y-%m-%d\ %H:%M:%S)"
+  fi
+  echo -n "${p_icon}$(date +%Y-%m-%d\ %H:%M:%S)"
 }
 
 
@@ -88,9 +99,9 @@ if [ "$HAS_TPUT" = "true" ] ; then
 		_RED=$(tput setaf 124)
 		_GREEN=$(tput setaf 46)
 		_YELLOW=$(tput setaf 192)
-		_BLUE=$(tput setaf 39)
+		_BLUE=$(tput setaf 25)
 		_MAGENTA=$(tput setaf 129)
-		_CYAN=$(tput setaf 39)
+		_CYAN=$(tput setaf 51)
 		_WHITE=$(tput setaf 15)
 	else
 		_BLACK=$(tput setaf 0)
@@ -133,18 +144,29 @@ export _RESET
 
 # Now build up all the pieces of the command prompt
 PS_TITLE="\[$TITLE\w\a\]"
-PS_BASIC="\[${_GREEN}\]\u@\h \[${_YELLOW}\]\w \[${_MAGENTA}\]\$(prompt_time)"
+PS_USER="\[${_GREEN}\]\u@\h"
+PS_DIR="\[${_YELLOW}\]\w"
 PS_ERROR="\[$_RED\]\$(parse_last_command_error)"
 PS_END="\n\[${_RESET}\]$ "
 
+PS_TIME="\[${_MAGENTA}\]\$(prompt_time)"
+
 # Extra stuff
 if [ "" != "`which git 2> /dev/null `" ] ; then
-    PS_GIT="\[${_CYAN}\]\$(parse_git_branch)"
+  PS_GIT="\[${_CYAN}\]\$(parse_git_branch)"
+else
+  PS_GIT=
 fi
 
-PS_AWS="\[${_WHITE}\]\$(parse_aws_info)"
+PS_AWS="\[${_WHITE}\]\$(parse_aws_info) "
+
+if [ "" != "`which rbenv 2> /dev/null `" ] ; then
+  PS_RUBY="\[${_BLUE}\]\$(parse_ruby_info)"
+else
+  PS_RUBY=
+fi
 
 # And put them all together
-PS1="${PS_TITLE}${PS_ERROR}\n${PS_BASIC} ${PS_GIT} ${PS_AWS} ${PS_EXTRAS} ${PS_END}"
+PS1="${PS_TITLE}${PS_ERROR}\n${PS_USER} ${PS_TIME}\n${PS_DIR} ${PS_GIT} ${PS_RUBY} ${PS_AWS} ${PS_EXTRAS} ${PS_END}"
 
 
