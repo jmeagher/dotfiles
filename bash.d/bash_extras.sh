baf0bd569ff0285ff93d331a37d0f65b997ed474
John Meagher <john.meagher@livingsocial.com>
2014-05-12 14:04:44 -0400

    Wide diff option

--- bash.d/bash_extras.sh
+++ bash.d/bash_extras.sh
@@ -12,3 +12,10 @@ nonascii() { LANG=C grep --color=always '[^ -~]\+'; }
 # History control, skip dups
 export HISTCONTROL=erasedups
 
+
+if [ "" != "`which tput 2> /dev/null `" ] ; then
+  alias diffw='diff -W $(( $(tput cols) - 2 ))'
+else
+  alias diffw='diff -W $(( $COLUMNS - 2 ))'
+fi
+
