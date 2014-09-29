66acea490180ca409cbe8eae7d2dcba0f2e02dd8
John Meagher <jmeagher@twitter.com>
2014-09-29 10:55:34 -0700

    Bash completion extra update to be more homebrew friendly

--- bash.d/bash_completion.sh
+++ bash.d/bash_completion.sh
@@ -4,11 +4,19 @@ for file in /usr/local/etc/bash_completion /opt/local/etc/bash_completion /etc/b
 	[ -r "$file" ] && source "$file"
 done
 
+# Special homebrew version
+if [ "" != "`which brew 2> /dev/null `" ] ; then
+  if [ -f $(brew --prefix)/etc/bash_completion ]; then
+    . $(brew --prefix)/etc/bash_completion
+  fi
+fi
+
 # run any user completion setup
 if [ -d ~/.bash_completion.d ] ; then
     for file in ~/.bash_completion.d/* ; do
         [ -r "$file" ] && source "$file"
     done
 fi
+
 unset file
 
