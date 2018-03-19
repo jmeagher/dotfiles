86c12bc43466b6bc6d36fcc626ec6263e5a411aa
John Meagher <john.meagher@gmail.com>
2018-03-18 17:16:46 -0700

    Minor tweak for rvm

--- bash.d/ruby.sh
+++ bash.d/ruby.sh
@@ -106,8 +106,8 @@ else
 fi
 
 
-if [ "" != "`which rvm 2> /dev/null `" ] ; then
-  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+  source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
   export PATH="$PATH:$HOME/.rvm/bin"
 fi
 
