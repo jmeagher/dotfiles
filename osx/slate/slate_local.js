6e785eae58efac8007a774305cfa0f195e168941
John Meagher <john.meagher@instacart.com>
2020-07-17 13:45:10 -0700

    Snapshot some recent changes

--- osx/slate/slate_local.js
+++ osx/slate/slate_local.js
@@ -29,12 +29,23 @@ var twoMonitorLayout = S.lay("twoMonitor", _.extend(commonLayout(), {
     ], cornerSize(horizontal, "top-right", 0.5, 0.7)),
 }));
 
+var twoMonitor2Layout = S.lay("twoMonitor2", _.extend(commonLayout(), {
+  "MacVim"        : w(cornerOnly(mainScreen, "top-left")),
+  "iTerm2"        : w(cornerSize(horizontal2, "bottom-left", 0.7, 0.8)),
+  "IntelliJ IDEA" : w(cornerSize(mainScreen, "top-right", 0.9, 0.9)),
+  "Code"          : w(cornerSize(mainScreen, "top-right", 0.9, 0.9)),
+  "Google Chrome" : byTitleRegex([
+    {"r":pbr, "op":cornerSize(horizontal2, "bottom-right", 0.6, 0.7)}
+    ], cornerSize(horizontal2, "top-left", 0.8, 0.9)),
+}));
+
 // Defaults
-S.def(2, twoMonitorLayout);
+S.def(2, twoMonitor2Layout);
 S.def(1, oneMonitorLayout);
 
 // Layout Operations
 var twoMonitor = S.op("layout", { "name" : twoMonitorLayout });
+var twoMonitor2 = S.op("layout", { "name" : twoMonitor2Layout });
 var oneMonitor = S.op("layout", { "name" : oneMonitorLayout });
 
 var relaunch = S.operation("relaunch");
@@ -45,6 +56,7 @@ S.bnda({
   // Layout Bindings
   "1:ctrl;cmd;alt" : function() { oneMonitor.run(); },
   "2:ctrl;cmd;alt" : function() { twoMonitor.run(); },
+  "3:ctrl;cmd;alt" : function() { twoMonitor2.run(); },
   "5:ctrl;cmd;alt" : relaunch,
 
 });
