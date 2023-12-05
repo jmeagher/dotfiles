4b12533fd6b57a9ad061e136398bebeb6a12c02d
John Meagher <github@jmeagher.net>
2023-12-04 23:27:10 -0600

    A few more tweaks

--- setup/basic_tools.sh
+++ setup/basic_tools.sh
@@ -2,8 +2,8 @@ sudo apt-get update
 
 # Generally useful dev stuff
 sudo apt-get install -y \
-  coreutils manpages man-db \
-  python3 python3-distutils python3-dev \
+  coreutils manpages man-db build-essential \
+  python3 python3-distutils python3-dev python3-venv \
   bash-completion \
   tmux screen \
   silversearcher-ag jq \
---
+++ setup/zsh.sh
@@ -0,0 +1,5 @@
+if [ ! -e ~/.oh-my-zsh ]
+then
+  echo "Installing oh-my-zsh"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
